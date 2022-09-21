4ea9b4b25710dfa0a8c1e2451bfd761ea1e44700
venkat143-v <81428675+venkat143-v@users.noreply.github.com>
2022-09-21 21:41:04 +0530

    Create temp.java

---
+++ temp.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.lang.Math;
+class Main{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        float a=s.nextFloat();
+        double f;
+        f=32+(a*9/5);
+        System.out.printf("%.2f",f);
+    }
+}
